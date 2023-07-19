0522ddf16610a09e96985dc11f7fd140316b099b
03febs <febrietsani03@gmail.com>
2023-07-19 14:45:06 +0700

    add wrap widget

--- lib/pages/febrie/febrie_page.dart
+++ lib/pages/febrie/febrie_page.dart
@@ -10,6 +10,119 @@ class FebriePage extends StatelessWidget {
         title: const Text('U App'),
         backgroundColor: Colors.green,
       ),
+      body: Wrap(
+        alignment: WrapAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyanAccent,
+            margin: const EdgeInsets.all(5),
+          ),
+        ],
+      ),
     );
   }
 }
