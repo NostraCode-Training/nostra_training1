984488739c9e3f82c261a616228f9a6ca5138559
annisa <annisaputriwahyuni46@gmail.com>
2023-07-19 14:20:37 +0700

    routing pages

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:nostra_training1/pages/febrie/febrie_page.dart';
+import 'package:nostra_training1/pages/gia/gia_page.dart';
+import 'package:nostra_training1/pages/nadhilah/nadhilah_page.dart';
+import 'package:nostra_training1/pages/qisthi/qisthi_page.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,10 +10,53 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                final route = MaterialPageRoute(
+                  builder: (context) => const GiaPage(),
+                );
+                Navigator.push(context, route);
+              },
+              child: const Text('Gia Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final route = MaterialPageRoute(
+                  builder: (context) => const FebriePage(),
+                );
+                Navigator.push(context, route);
+              },
+              child: const Text('Febrie Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final route = MaterialPageRoute(
+                  builder: (context) => const NadhilahPage(),
+                );
+                Navigator.push(context, route);
+              },
+              child: const Text('Nadhilah Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final route = MaterialPageRoute(
+                  builder: (context) => const QisthiPage(),
+                );
+                Navigator.push(context, route);
+              },
+              child: const Text('Qisthi Page'),
+            ),
+          ],
         ),
-        body: Container());
+      ),
+    );
   }
 }
---
+++ lib/pages/qisthi/qisthi_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class QisthiPage extends StatelessWidget {
+  const QisthiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Qisthi Page'),
+      ),
+    );
+  }
+}
