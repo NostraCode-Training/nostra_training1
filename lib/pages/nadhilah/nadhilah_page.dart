c92b61a0d34d1d8499cbcef48a2c6345d2417018
nadhilahaz <hazratinadhilah@gmail.com>
2023-07-19 14:44:58 +0700

    add wrap widget

--- lib/pages/nadhilah/nadhilah_page.dart
+++ lib/pages/nadhilah/nadhilah_page.dart
@@ -9,6 +9,101 @@ class NadhilahPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Nadhilah Hazrati Page'),
       ),
+      body: Wrap(
+        alignment: WrapAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.yellow,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.pink,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.purple,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.yellow,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.pink,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.purple,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.yellow,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.pink,
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: 100,
+            width: 100,
+            color: Colors.purple,
+          ),
+        ],
+      ),
     );
   }
 }
